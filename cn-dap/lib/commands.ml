df378e2b31e02c552b1c42e8604fd34e05eb084f
Sam Cowger <sam@galois.com>
2024-10-30 12:19:22 -0700

    debugger: organize command modules together

--- cn-dap/lib/commands.ml
+++ cn-dap/lib/commands.ml
@@ -1,7 +1,26 @@
 open Base
 module Dap = Debug_protocol
 
-module Command = struct
+module DebuggerState = struct
+  let type_ = "debuggerState"
+
+  module Arguments = struct
+    type t = unit
+
+    let of_yojson = function
+      | `Assoc [] -> Ok ()
+      | _ -> Error "non-null"
+    ;;
+
+    let to_yojson () = `Assoc []
+  end
+
+  module Result = struct
+    type t = Debugger.WireState.t [@@deriving yojson]
+  end
+end
+
+module Launch = struct
   let type_ = Dap.Launch_command.type_
 
   module Arguments = struct
