b31387b2f0fd85b99ddadb433ff916941434bdf7
Sam Cowger <sam@galois.com>
2024-12-16 12:06:59 -0800

    telemetry: test multiple sessions

--- telemetry/test/storage.ml
+++ telemetry/test/storage.ml
@@ -46,9 +46,37 @@ let test_store_load_twice () =
   Alcotest.(check (list event_t)) "loaded vs. stored" [ event1; event2 ] load_result
 ;;
 
+let test_store_load_two_sessions () =
+  let session1 = Session.custom () in
+  let session2 = Session.today () in
+  let event1 = Event.create ~source ~session:session1 ~event_data:SampleEvent.startup in
+  let event2 = Event.create ~source ~session:session2 ~event_data:SampleEvent.shutdown in
+  let () = ok (Storage.store storage ~event:event1 ~serialize:SampleEvent.to_yojson) in
+  let () = ok (Storage.store storage ~event:event2 ~serialize:SampleEvent.to_yojson) in
+  let session1_load_result =
+    ok
+      (Storage.load_session
+         storage
+         ~source:"testing"
+         ~session:session1
+         ~deserialize:SampleEvent.of_yojson)
+  in
+  Alcotest.(check (list event_t)) "loaded vs. stored" [ event1 ] session1_load_result;
+  let session2_load_result =
+    ok
+      (Storage.load_session
+         storage
+         ~source:"testing"
+         ~session:session2
+         ~deserialize:SampleEvent.of_yojson)
+  in
+  Alcotest.(check (list event_t)) "loaded vs. stored" [ event2 ] session2_load_result
+;;
+
 let tests =
   let tc = Alcotest.test_case in
   [ tc "store once then load" `Quick test_store_load
   ; tc "store twice then load" `Quick test_store_load_twice
+  ; tc "store in two sessions then load" `Quick test_store_load_two_sessions
   ]
 ;;
