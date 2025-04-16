ed6a6a352568a8a4abcf5ee09438a1c55d95a23d
Sam Cowger <sam@galois.com>
2025-04-16 09:15:39 -0700

    server: begin to test server functionality via RPC request/response

--- cn-lsp/test/main.ml
+++ cn-lsp/test/main.ml
@@ -1,1 +1,12 @@
-let () = Alcotest.run "cn-lsp" [ "Parse", Parse.tests ]
+open! Base
+
+let () =
+  let lwtify tests =
+    List.map tests ~f:(fun (n, s, f) -> Alcotest_lwt.test_case_sync n s f)
+  in
+  Lwt_main.run
+    (Alcotest_lwt.run
+       "cn-lsp"
+       ~verbose:true
+       [ "Parse", lwtify Parse.tests; "Server", Server.tests ])
+;;
